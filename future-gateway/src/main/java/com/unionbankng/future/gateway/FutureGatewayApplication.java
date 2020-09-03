f6cbe6d3e28c31058afefd185f9ceb5b5c836144
Okeme Christian <chokeme@unionbankng.com>
2020-09-04 00:04:44 +0100

    added swagger documentation

--- future-gateway/src/main/java/com/unionbankng/future/gateway/FutureGatewayApplication.java
+++ future-gateway/src/main/java/com/unionbankng/future/gateway/FutureGatewayApplication.java
@@ -2,8 +2,13 @@ package com.unionbankng.future.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class FutureGatewayApplication {
 
 	public static void main(String[] args) {
---
+++ future-gateway/src/main/java/com/unionbankng/future/gateway/config/SwaggerRegister.java
@@ -0,0 +1,34 @@
+package com.unionbankng.future.gateway.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Component
+@Primary
+@EnableAutoConfiguration
+public class SwaggerRegister implements SwaggerResourcesProvider {
+
+	@Override
+	public List<SwaggerResource> get() {
+		List<SwaggerResource> resources = new ArrayList<>();
+		resources.add(swaggerResource("authorization-server", "/authserv/v2/api-docs", "0.0.1"));
+
+		return resources;
+	}
+
+	private SwaggerResource swaggerResource(String name, String location, String version) {
+		SwaggerResource swaggerResource = new SwaggerResource();
+		swaggerResource.setName(name);
+		swaggerResource.setLocation(location);
+		swaggerResource.setSwaggerVersion(version);
+		return swaggerResource;
+	}
+
+}
