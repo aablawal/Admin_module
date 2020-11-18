5560c6e155dca967d54cf8bf87ce6ff225d714fb
Okeme Christian <cbokeme@unionbankng.com>
2020-11-18 19:12:08 +0100

    fixed issue with bank bvn links

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/security/SecurityConfig.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/security/SecurityConfig.java
@@ -28,12 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .formLogin()
-
-                .and()
-
-                .httpBasic().disable()
-                .anonymous().disable();
+                .formLogin();
     }
 
     @Bean
