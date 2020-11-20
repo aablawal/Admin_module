9b93e2311c533107841cb9911214f15e12b38a89
Okeme Christian <cbokeme@unionbankng.com>
2020-11-20 04:42:55 +0100

    added jssion in request

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/security/SecurityConfig.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -30,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
 
-                .and()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS).and()
 
                 .httpBasic().disable()
                 .anonymous().disable();
