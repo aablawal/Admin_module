e1f9935d62631a9ec5745588f88ed0bd9b84df88
Okeme Christian <cbokeme@unionbankng.com>
2020-11-22 11:33:12 +0100

    changed upload method for video streams to rest

--- future-learn-service/src/test/java/com/unionbankng/future/learn/LearnServiceApplicationTests.java
+++ future-learn-service/src/test/java/com/unionbankng/future/learn/LearnServiceApplicationTests.java
@@ -1,12 +1,9 @@
 package com.unionbankng.future.learn;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest
-class LearnServiceApplicationTests {
+
+class LearnServiceApplicationTests extends AbstractTest{
 
 	@Test
 	void contextLoads() {
