8f60f4a9ab0be87b215a39f3ff9d7ddc05dbc530
Okeme Christian <cbokeme@unionbankng.com>
2020-11-23 13:26:33 +0100

    added account number fields for paid courses

--- future-learn-service/src/main/java/com/unionbankng/future/learn/pojo/CreateCourseRequest.java
+++ future-learn-service/src/main/java/com/unionbankng/future/learn/pojo/CreateCourseRequest.java
@@ -3,6 +3,8 @@ package com.unionbankng.future.learn.pojo;
 import com.unionbankng.future.learn.entities.Instructor;
 import lombok.Builder;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -10,14 +12,24 @@ import java.util.List;
 @Builder
 public class CreateCourseRequest {
 
+    @NotNull
     private String description;
+    @NotNull
     private String requirements;
+    @NotNull
     private Boolean isPaid;
     private BigDecimal price;
+    @NotNull
     private String courseTitle;
+    @NotNull
     private String shortDesc;
     private String outcomes;
+    @NotNull
     private Boolean isPublished;
     private List<Instructor> instructors;
+    @NotNull
     private String estimatedTimeToComplete;
+    private String accountNumber;
+    private String accountName;
+    private String paymentTerms;
 }
