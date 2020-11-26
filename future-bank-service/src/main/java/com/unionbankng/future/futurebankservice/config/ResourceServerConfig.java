0fd57a0d314123a4b9971015e79bacab3210a6b4
Okeme Christian <cbokeme@unionbankng.com>
2020-11-26 10:02:48 +0100

    added a more robust method to extract accountNumber from account creation response

--- future-bank-service/src/main/java/com/unionbankng/future/futurebankservice/config/ResourceServerConfig.java
+++ future-bank-service/src/main/java/com/unionbankng/future/futurebankservice/config/ResourceServerConfig.java
@@ -32,7 +32,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 
-		http.cors().and().requestMatchers().antMatchers("/api/**").and().authorizeRequests().antMatchers("/api/**").authenticated();
+		http.cors().and().requestMatchers().antMatchers("/api/**").and().authorizeRequests().anyRequest().authenticated();
 
 	}
 
