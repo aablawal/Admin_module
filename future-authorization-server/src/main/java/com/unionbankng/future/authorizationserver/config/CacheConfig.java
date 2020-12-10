3bb4d66272a5e7856a4dea52dcaed7c6dedd59a9
Okeme Christian <cbokeme@unionbankng.com>
2020-12-10 19:42:51 +0100

    added to update cover and profile image in one call

---
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package com.unionbankng.future.authorizationserver.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+@Profile("!test")
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/config/MemcachedConfig.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/config/MemcachedConfig.java
@@ -19,13 +19,14 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 
+
 @Configuration
-@EnableCaching
 public class MemcachedConfig extends AbstractSSMConfiguration {
 
     @Value( "${memcached.service.host}" )
