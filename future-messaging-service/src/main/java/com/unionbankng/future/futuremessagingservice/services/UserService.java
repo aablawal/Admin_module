2d928822fb7c9bac5c82d7970de2a75919abf84a
rabs-developer <e.rabiualiyu@gmail.com>
2020-12-11 11:11:56 +0100

    GRPC connection fixes

--- future-messaging-service/src/main/java/com/unionbankng/future/futuremessagingservice/services/UserService.java
+++ future-messaging-service/src/main/java/com/unionbankng/future/futuremessagingservice/services/UserService.java
@@ -16,27 +16,30 @@ public class UserService {
     Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public User getUserById(Long userId){
-        UserDetailRequest request = UserDetailRequest.newBuilder().
-        setUserId(userId).build();
-        UserDetailResponse response=sidekiqUserDetailServiceStub.getUserDetail(request);
-         User user = new User();
-         user.setId(response.getId());
-         user.setUuid(response.getUuid());
-         user.setFullName(response.getFullName());
-         user.setCountry(response.getCountry());
-         user.setAddress(response.getAddress());
-         user.setEmail(user.getEmail());
-         user.setPhoneNumber(response.getPhoneNumber());
-         user.setStateOfResidence(response.getStateOfResidence());
-         user.setUsername(user.getUsername());
-         user.setImg(user.getImg());
-         user.setAccountName(user.getAccountName());
-         user.setAccountNumber(user.getAccountNumber());
-         user.setIsEnabled(user.getIsEnabled());
-         user.setCreatedAt(user.getCreatedAt());
-
-         logger.info(user.toString());
-       return user;
+        try {
+            UserDetailRequest request = UserDetailRequest.newBuilder().
+                    setUserId(userId).build();
+            UserDetailResponse response = sidekiqUserDetailServiceStub.getUserDetail(request);
+            User user = new User();
+            user.setId(response.getId());
+            user.setUuid(response.getUuid());
+            user.setFullName(response.getFullName());
+            user.setCountry(response.getCountry());
+            user.setAddress(response.getAddress());
+            user.setEmail(user.getEmail());
+            user.setPhoneNumber(response.getPhoneNumber());
+            user.setStateOfResidence(response.getStateOfResidence());
+            user.setUsername(user.getUsername());
+            user.setImg(user.getImg());
+            user.setAccountName(user.getAccountName());
+            user.setAccountNumber(user.getAccountNumber());
+            user.setIsEnabled(user.getIsEnabled());
+            user.setCreatedAt(user.getCreatedAt());
+            return user;
+        }catch (Exception e){
+            e.printStackTrace();
+            return  null;
+        }
     }
 }
 
