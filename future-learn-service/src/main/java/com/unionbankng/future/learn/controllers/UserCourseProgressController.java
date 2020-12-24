380551d68912a2c7199fe951d3cc2a5713e0392c
Okeme Christian <cbokeme@unionbankng.com>
2020-12-24 09:17:40 +0100

    added validation for course progress

--- future-learn-service/src/main/java/com/unionbankng/future/learn/controllers/UserCourseProgressController.java
+++ future-learn-service/src/main/java/com/unionbankng/future/learn/controllers/UserCourseProgressController.java
@@ -1,15 +1,10 @@
 package com.unionbankng.future.learn.controllers;
 
-import com.unionbankng.future.learn.entities.Course;
-import com.unionbankng.future.learn.entities.Lecture;
 import com.unionbankng.future.learn.entities.UserCourseProgress;
 import com.unionbankng.future.learn.pojo.APIResponse;
-import com.unionbankng.future.learn.pojo.CreateCourseRequest;
 import com.unionbankng.future.learn.pojo.UserCourseProgressRequest;
 import com.unionbankng.future.learn.services.UserCourseProgressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
