a10a5fc0d703154b30e0f814f6770edff4ae45e1
Okeme Christian <cbokeme@unionbankng.com>
2021-01-20 10:05:21 +0100

    trying out spring cloud stream

--- future-messaging-service/src/main/java/com/unionbankng/future/futuremessagingservice/jmslisteners/EmailListener.java
+++ future-messaging-service/src/main/java/com/unionbankng/future/futuremessagingservice/jmslisteners/EmailListener.java
@@ -8,6 +8,7 @@ import com.unionbankng.future.futuremessagingservice.smsandemailproviders.UnionE
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
@@ -31,7 +32,6 @@ public class EmailListener {
 
     @Bean
     public Consumer<String> sendEmail(){
-
         return emailRequest ->{
             try {
                 logger.info("Received message: {}", emailRequest);
