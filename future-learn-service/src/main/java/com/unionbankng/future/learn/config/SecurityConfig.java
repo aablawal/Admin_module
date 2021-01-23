a9c47cbee6b49b22d3988f1b1a868261f7611503
Okeme Christian <cbokeme@unionbankng.com>
2021-01-23 08:20:57 +0100

    fix

--- future-learn-service/src/main/java/com/unionbankng/future/learn/config/SecurityConfig.java
+++ future-learn-service/src/main/java/com/unionbankng/future/learn/config/SecurityConfig.java
@@ -2,14 +2,15 @@ package com.unionbankng.future.learn.config;
 
 import org.keycloak.adapters.KeycloakConfigResolver;
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
@@ -25,14 +26,13 @@ import java.util.Arrays;
 
 @Component
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.cors().and().requestMatchers().antMatchers("/api*").and().authorizeRequests().anyRequest().authenticated();
-        http.csrf().disable();
 
     }
 
