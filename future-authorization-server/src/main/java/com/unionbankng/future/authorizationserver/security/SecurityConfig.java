a29a4744e07747e8232d9306944d240e2388c18d
Rabiu Aliyu <net.rabiualiyu@gmail.com>
2021-03-05 10:30:08 +0100

    Fixes

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/security/SecurityConfig.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.cors().and().csrf().disable().authorizeRequests().antMatchers("/login","/oauth/token", "/oauth/authorize/**","/api/v1/registration/**",
-				"/api/v1/security/**","/api/v1/tags/find_by_type","/api/v1/user_interest/add_for_user","/api/v1/tags/find_by_type_likely_name","/api/v1/tags/create").permitAll()
+				"/api/v1/security/**","/api/v1/tags/find_by_type","/api/v1/tags/find_by_id","/api/v1/user_interest/add_for_user","/api/v1/tags/find_by_type_likely_name","/api/v1/tags/create").permitAll()
 				.and().requestMatchers().antMatchers("/api/**")
 				.and().authorizeRequests()
 				.antMatchers("/api/**").authenticated();
