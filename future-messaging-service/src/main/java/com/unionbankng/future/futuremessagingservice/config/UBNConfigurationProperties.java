ee02cf781bec704fef4ed0551a98c18d3cd9f6f9
Rabiu Aliyu <net.rabiualiyu@gmail.com>
2021-07-28 16:18:33 +0100

    Email Fixes

--- future-messaging-service/src/main/java/com/unionbankng/future/futuremessagingservice/config/UBNConfigurationProperties.java
+++ future-messaging-service/src/main/java/com/unionbankng/future/futuremessagingservice/config/UBNConfigurationProperties.java
@@ -41,7 +41,7 @@ public class UBNConfigurationProperties {
     }
 
 
-    @Value("${unionbankng.credentials}")
+    @Value("#{${unionbankng.credentials}}")
     public void setCredentials(Map<String, String> credentials) {
         CLIENT_SECRET = credentials.get("clientSecret");
         CLIENT_ID = credentials.get("clientId");
