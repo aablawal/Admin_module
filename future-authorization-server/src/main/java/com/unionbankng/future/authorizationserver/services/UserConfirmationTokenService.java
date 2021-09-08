76c2a538ef215dd23de77559ce5d053e25e28591
Rabiu Aliyu <e.rabiualiyu@gmail.com>
2021-09-08 17:20:24 +0000

    Fixes

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/services/UserConfirmationTokenService.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/services/UserConfirmationTokenService.java
@@ -42,6 +42,14 @@ public class UserConfirmationTokenService {
     @Value("${confirmation.token.url}")
     private String confirmationTokenURL;
 
+    public void sendEmailTest(String email) {
+        EmailBody emailBody = EmailBody.builder().body(messageSource.getMessage("welcome.message", new String[]{"Email test from kula app"}, LocaleContextHolder.getLocale())
+                ).sender(EmailAddress.builder().displayName("Kula Team").email(emailSenderAddress).build()).subject("Kula Test")
+                .recipients(Arrays.asList(EmailAddress.builder().recipientType(RecipientType.TO).email(email).displayName("Kula User").build())).build();
+
+        emailSender.sendEmail(emailBody);
+        logger.info("Message Queued successfully");
+    }
     public void sendConfirmationToken(User user) {
         try {
             logger.info("=============================");
