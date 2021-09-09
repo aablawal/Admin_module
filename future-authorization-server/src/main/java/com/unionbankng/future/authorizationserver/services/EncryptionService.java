5fc2cc35b889c7e7e2a6fa8606993c1edeed43e0
Rabiu Aliyu <e.rabiualiyu@gmail.com>
2021-09-09 07:18:58 +0000

    Fixes

---
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/services/EncryptionService.java
@@ -0,0 +1,70 @@
+package com.unionbankng.future.authorizationserver.services;
+
+import java.security.spec.KeySpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EncryptionService {
+
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    private KeySpec ks;
+    private SecretKeyFactory skf;
+    private Cipher cipher;
+    byte[] arrayBytes;
+    private String myEncryptionKey;
+    private String myEncryptionScheme;
+    SecretKey key;
+
+    public EncryptionService() throws Exception {
+       myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+        ks = new DESedeKeySpec(arrayBytes);
+        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
+        cipher = Cipher.getInstance(myEncryptionScheme);
+        key = skf.generateSecret(ks);
+    }
+
+
+    public String encrypt(String text, String password) {
+        String encryptedString = null;
+        myEncryptionKey = password;
+
+        try {
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = text.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.encodeBase64(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+
+
+    public String decrypt(String cypher, String password) {
+        String decryptedText=null;
+        myEncryptionKey = password;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.decodeBase64(cypher);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText= new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
+
+
+}
--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/services/PinService.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/services/PinService.java
@@ -4,17 +4,10 @@ import com.unionbankng.future.authorizationserver.entities.User;
 import com.unionbankng.future.authorizationserver.pojos.APIResponse;
 import com.unionbankng.future.authorizationserver.pojos.JwtUserDetail;
 import com.unionbankng.future.authorizationserver.repositories.UserRepository;
-import com.unionbankng.future.authorizationserver.utils.App;
 import com.unionbankng.future.authorizationserver.utils.JWTUserDetailsExtractor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.Principal;
 
 @Service
@@ -22,29 +15,29 @@ import java.security.Principal;
 public class PinService {
 
     @Value("${kula.encryption.key}")
-    private int key=256;
+    private String encryptionKey="Rabs@1994";
     private final UserRepository userRepository;
-    private final App app;
+    private final EncryptionService encryptionService;
 
-    public APIResponse createPin(Principal principal, String pin) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public APIResponse createPin(Principal principal, String pin) {
         System.out.println("###########Creating PIN");
-        System.out.println("Key:"+key);
+        System.out.println("Key:"+encryptionKey);
         System.out.println("Pin:"+pin);
         JwtUserDetail currentUser = JWTUserDetailsExtractor.getUserDetailsFromAuthentication(principal);
         User user=userRepository.findByUuid(currentUser.getUserUUID()).orElse(null);
         if(user!=null){
-            user.setPin((app.encrypt(pin,key)));
+            user.setPin((encryptionService.encrypt(pin,encryptionKey)));
             return new APIResponse<>("Pin Added Successfully", true,   userRepository.save(user));
         }else{
             return new APIResponse<>("Account not found", false, null);
         }
     }
 
-    public APIResponse verifyPin(Principal principal, String pin) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+    public APIResponse verifyPin(Principal principal, String pin){
         JwtUserDetail currentUser = JWTUserDetailsExtractor.getUserDetailsFromAuthentication(principal);
         User user=userRepository.findByUuid(currentUser.getUserUUID()).orElse(null);
         if(user!=null){
-            String existingPin=app.decrypt(user.getPin(),key);
+            String existingPin=encryptionService.decrypt(user.getPin(),encryptionKey);
             String providedPin=pin;
             System.out.println("##########Pin Verification started");
             System.out.println("Existing:"+existingPin);
