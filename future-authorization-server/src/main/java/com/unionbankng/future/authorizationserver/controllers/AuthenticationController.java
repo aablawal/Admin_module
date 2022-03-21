b2b5559fe5e7fb16e3af014a59bc57b4a0b3d15d
firsthus <audufirsthus@gmail.com>
2022-03-21 02:16:43 +0100

    implemented reset pin api

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/AuthenticationController.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/AuthenticationController.java
@@ -1,10 +1,13 @@
 package com.unionbankng.future.authorizationserver.controllers;
 import com.unionbankng.future.authorizationserver.pojos.APIResponse;
+import com.unionbankng.future.authorizationserver.pojos.ResetPinRequest;
 import com.unionbankng.future.authorizationserver.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @RestController
@@ -17,6 +20,17 @@ public class AuthenticationController {
     @PostMapping("/v1/pin/set")
     public ResponseEntity<APIResponse> createNewPin(OAuth2Authentication authentication, @RequestParam String pin){
         return ResponseEntity.ok().body(authenticationService.createPin(authentication, pin));
+    }
+
+    @PostMapping("v1/pin/initiate-forgot-pin")
+    public ResponseEntity<?> initiateForgotPin(@RequestParam String email){
+        return ResponseEntity.ok().body(authenticationService.initiateForgotPin(email));
+    }
+
+
+    @PostMapping("v1/pin/reset-pin")
+    public ResponseEntity<?> resetPassword(@RequestBody @Valid ResetPinRequest request){
+        return authenticationService.resetPin(request.getToken(), request.getNewPin());
     }
 
     @PostMapping("/v1/pin/verify")
@@ -33,5 +47,6 @@ public class AuthenticationController {
     public ResponseEntity<APIResponse> verifyOTP(OAuth2Authentication authentication, @RequestParam String otp){
         return ResponseEntity.ok().body(authenticationService.verifyOTP(authentication, otp));
     }
+
 
 }
