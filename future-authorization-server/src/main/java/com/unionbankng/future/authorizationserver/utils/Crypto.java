1bcf0d61edd1c9c7d953ab9d1fb9776d23ab91b3
firsthus <audufirsthus@gmail.com>
2022-06-21 10:57:15 +0100

    fixes

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/utils/Crypto.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/utils/Crypto.java
@@ -5,28 +5,28 @@ import java.util.Map;
 
 public class Crypto {
 
-    public static  void main(String args[]) throws JsonProcessingException {
-        CryptoService cryptoService = new CryptoService();
-        App app = new App();
-
-        String data="{\"clientSecret\":\"KULAUSERCLIENT\", \"clientId\": \"KULAUSER\",\"grantType\":\"password\",\"username\":\"kulasuser\",\"password\":\"Fibba21\"}";
-        String accessKey="KULAJLkwxFqcj5ojGqdjHdSYaFWdBprD";
-        String cypher="bOLmaKWtD6Lu2WKhR146bNssy3YdrFZrKG3lv4nLS9AKaEcXEzPP++TvpOvYgIblcu17lilPIMVa+JLkwxFqcj5ojGqdjHdSYaFWdBprDSZAIwHOtZVdNOzugw5zbyZr8/UMe9wfZs09uVwWbcnNhkHN7qrQzM46lCJqJiOUEeQQq8ku1k9jW4AoeSVKSyh95uOfOZnTWk5bGHYdirglyg==";
-        String encrypted= cryptoService.encrypt(data,accessKey);
-//        Map<String, String> cypherO = app.getMapper().readValue(encrypted, Map.class);
+//    public static  void main(String args[]) throws JsonProcessingException {
+//        CryptoService cryptoService = new CryptoService();
+//        App app = new App();
 //
-        app.print(encrypted);
-
-//        try {
-//            String cypher=cryptoService.decrypt(encrypted, accessKey);
+//        String data="{\"clientSecret\":\"KULAUSERCLIENT\", \"clientId\": \"KULAUSER\",\"grantType\":\"password\",\"username\":\"kulasuser\",\"password\":\"Fibba21\"}";
+//        String accessKey="KULAJLkwxFqcj5ojGqdjHdSYaFWdBprD";
+//        String cypher="bOLmaKWtD6Lu2WKhR146bNssy3YdrFZrKG3lv4nLS9AKaEcXEzPP++TvpOvYgIblcu17lilPIMVa+JLkwxFqcj5ojGqdjHdSYaFWdBprDSZAIwHOtZVdNOzugw5zbyZr8/UMe9wfZs09uVwWbcnNhkHN7qrQzM46lCJqJiOUEeQQq8ku1k9jW4AoeSVKSyh95uOfOZnTWk5bGHYdirglyg==";
+//        String encrypted= cryptoService.encrypt(data,accessKey);
+////        Map<String, String> cypherO = app.getMapper().readValue(encrypted, Map.class);
+////
+//        app.print(encrypted);
 //
-//            Map<String, String> cypherO = app.getMapper().readValue(cypher, Map.class);
-//            System.out.println(cypherO.get("grantType"));
-//        }catch (Exception ex){
-//            app.print("Unable to decode cypher");
-//            ex.printStackTrace();
-//        }
-
-
-    }
+////        try {
+////            String cypher=cryptoService.decrypt(encrypted, accessKey);
+////
+////            Map<String, String> cypherO = app.getMapper().readValue(cypher, Map.class);
+////            System.out.println(cypherO.get("grantType"));
+////        }catch (Exception ex){
+////            app.print("Unable to decode cypher");
+////            ex.printStackTrace();
+////        }
+//
+//
+//    }
 }
