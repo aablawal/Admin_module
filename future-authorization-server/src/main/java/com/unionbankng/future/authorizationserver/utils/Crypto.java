eb0d1e63585b2c04df9432c9dcc9ba7ea0a80538
aliyura <e.rabiualiyu@gmail.com>
2022-07-08 16:25:55 +0100

    Fixes

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/utils/Crypto.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/utils/Crypto.java
@@ -9,13 +9,13 @@ public class Crypto {
         CryptoService cryptoService = new CryptoService();
         App app = new App();
 
-        String data="{\"clientSecret\":\"KULAUSERCLIENT\", \"clientId\": \"KULAUSER\",\"grantType\":\"password\",\"username\":\"kulasuser\",\"password\":\"Fibba21\"}";
+        String data="{\"clientSecret\":\"KULAUSERCLIENT\", \"clientId\": \"KULAUSER\",\"grantType\":\"password\",\"username\":\"kulasuser\",\"password\":\"KULig2qwkOQBMgXHjvI\"}";
         String accessKey="KULAJLkwxFqcj5ojGqdjHdSYaFWdBprD";
-        String cypher="bOLmaKWtD6Lu2WKhR146bNssy3YdrFZrKG3lv4nLS9AKaEcXEzPP++TvpOvYgIblcu17lilPIMVa+JLkwxFqcj5ojGqdjHdSYaFWdBprDSZAIwHOtZVdNOzugw5zbyZr8/UMe9wfZs09uVwWbcnNhkHN7qrQzM46lCJqJiOUEeQQq8ku1k9jW4AoeSVKSyh95uOfOZnTWk5bGHYdirglyg==";
-        String encrypted= cryptoService.encrypt(data,accessKey);
-//        Map<String, String> cypherO = app.getMapper().readValue(encrypted, Map.class);
+        String cypher="J0unJIaBwVXt/YMQUi/aKtwQkQnKO21kfVBSHn3VYHpzwRpysIyzhAWcfKxOS4BzVR6pRFZ5SDyf3yhCL5M5K9JoSpQmJVUc2fjZFC8yXUGKsOwfXDa21dLaC/khcXHUpBvz47mTyQEOVUZUAq3FQGj9pXnoBQQs3ZEMLGwXg/9onYYZmPG+IRrFoyhJsrWn";
+        String encrypted= cryptoService.decrypt(cypher,accessKey);
+      Map<String, String> cypherO = app.getMapper().readValue(encrypted, Map.class);
 //
-        app.print(encrypted);
+        app.print(cypherO);
 
 //        try {
 //            String cypher=cryptoService.decrypt(encrypted, accessKey);
