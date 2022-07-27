94519ac0d1e74c18b843e3bb7a5c94fafdd79664
kechinaka <fichukwurah@unionbankng.com>
2022-07-27 11:17:38 +0100

    Tier 2 kyc

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/pojos/DriversLicenceFaceMatchRequest.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/pojos/DriversLicenceFaceMatchRequest.java
@@ -8,11 +8,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DriversLicenceFaceMatchRequest {
-    private String idNo;
-    private String surname;
-    private String firstname;
-    private String dob;
-    private String passportBase64String;
-    private String idBase64String;
+//    private String idNo;
+//    private String surname;
+//    private String firstname;
+//    private String dob;
+//    private String passportBase64String;
+//    private String idBase64String;
+
+    private String photoUrl;
+    private String photo;
+    private String idType;
+    private String idNumber;
 
 }
