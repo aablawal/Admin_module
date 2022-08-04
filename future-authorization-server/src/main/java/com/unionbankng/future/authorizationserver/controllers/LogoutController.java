19aefcd0fd17ca8b84caa5c877b80e42353b4178
joannaebreso2 <jbebreso@unionbankng.com>
2022-08-04 13:09:07 +0100

    logout activity logs fix

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/LogoutController.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/LogoutController.java
@@ -56,13 +56,13 @@ public class LogoutController {
 
     }
 
-    @GetMapping("/v1/get_sessionId")
-    public ResponseEntity<APIResponse<String>> getSessionId(OAuth2Authentication auth) {
-
-        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
-        return ResponseEntity.ok().body(new APIResponse("Logout successful",true,details.getSessionId()));
-
-    }
+//    @GetMapping("/v1/get_sessionId")
+//    public ResponseEntity<APIResponse<String>> getSessionId(OAuth2Authentication auth) {
+//
+//        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
+//        return ResponseEntity.ok().body(new APIResponse("Logout successful",true,details.getSessionId()));
+//
+//    }
 
 
 }
