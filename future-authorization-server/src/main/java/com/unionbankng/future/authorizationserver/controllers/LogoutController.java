88510852c38e0a6f0c62756ff06d2940dce5d0a6
joannaebreso2 <jbebreso@unionbankng.com>
2022-08-04 12:52:30 +0100

    fixed notification for job

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/LogoutController.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/LogoutController.java
@@ -3,6 +3,7 @@ package com.unionbankng.future.authorizationserver.controllers;
 import com.unionbankng.future.authorizationserver.pojos.APIResponse;
 import com.unionbankng.future.authorizationserver.pojos.ActivityLog;
 import com.unionbankng.future.authorizationserver.pojos.JwtUserDetail;
+import com.unionbankng.future.authorizationserver.utils.App;
 import com.unionbankng.future.authorizationserver.utils.AppLogger;
 import com.unionbankng.future.authorizationserver.utils.JWTUserDetailsExtractor;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,8 @@ import java.util.Date;
 @RequiredArgsConstructor
 @RequestMapping(path = "api")
 public class LogoutController {
+
+    private final App app;
 
     private final ConsumerTokenServices tokenServices;
 
@@ -52,13 +56,13 @@ public class LogoutController {
 
     }
 
-//    @GetMapping("/v1/get_sessionId")
-//    public ResponseEntity<APIResponse<String>> getSessionId(OAuth2Authentication auth) {
-//
-//        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
-//        return ResponseEntity.ok().body(new APIResponse("Logout successful",true,details.getSessionId()));
-//
-//    }
+    @GetMapping("/v1/get_sessionId")
+    public ResponseEntity<APIResponse<String>> getSessionId(OAuth2Authentication auth) {
+
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
+        return ResponseEntity.ok().body(new APIResponse("Logout successful",true,details.getSessionId()));
+
+    }
 
 
 }
