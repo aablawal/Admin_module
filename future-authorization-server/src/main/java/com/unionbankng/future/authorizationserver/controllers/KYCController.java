515d2a77b9751b7ec8fc060fb9c2a2d055c17f0f
kechinaka <fichukwurah@unionbankng.com>
2022-08-11 08:43:33 +0100

    Validate multipart required fals

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/KYCController.java
+++ future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/controllers/KYCController.java
@@ -38,7 +38,6 @@ public class KYCController {
     private final UserRepository userRepository;
     private final App app;
 
-
     @PostMapping("/v1/kyc/initiation")
     public APIResponse<Map<String, String>> initiateKYC(OAuth2Authentication authentication, @RequestParam String bvn, @RequestParam(required = false) String dob) {
 
