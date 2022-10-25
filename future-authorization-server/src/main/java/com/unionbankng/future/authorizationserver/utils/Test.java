41dd4cbf35682218e976b8cafa775da8cfd19b0e
aliyura <e.rabiualiyu@gmail.com>
2022-10-25 08:54:18 +0100

    Fixes

--- future-authorization-server/src/main/java/com/unionbankng/future/authorizationserver/utils/Test.java
+++
@@ -1,9 +0,0 @@
-package com.unionbankng.future.authorizationserver.utils;
-
-public class Test {
-
-    public static  void main(String args[]){
-        CryptoService cryptoService = new CryptoService();
-        cryptoService.de
-    }
-}
